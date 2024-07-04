b90febbde6cd331552b3d20e72d9dd032609d03c
Pasha <pasha@getinkworks.com>
2024-07-03 22:09:41 -0700

    testing vite base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	base: '/portfolio-v2',
+	// base: '/portfolio-v2',
 	plugins: [react()],
 	resolve: {
 		alias: {
